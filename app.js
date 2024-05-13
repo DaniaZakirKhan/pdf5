aaf66ae99aa6fe47f2b74c4e9c4fd11f76fc013d
Dania Zakir <zakirdania3@gmail.com>
2024-05-13 18:45:17 +0500

    add app.js or index.html

---
+++ app.js
@@ -0,0 +1,38 @@
+
+var num1 = 5;
+var num2 = 3;
+
+var division = num1 / num2;
+
+document.write("Division of " + num1 + " by " + num2 + " is: " + division+"</br>");
+
+var num1=3;
+var num2=5;
+var exponent = num1 ** num2;
+ document.write(" exponent of " + num1 + " and " +num2 +" is: "+ exponent+"</br>");
+
+
+
+
+
+ var num1=3;
+var num2=5;
+
+var sum=num1+num2;
+
+document.write(" Sum of  "  + num1 +"  and " +num2 +" is: "+sum+"</br>");
+
+var num1=5;
+var num2=3;
+
+var sub=num1-num2;
+
+document.write(" Sub of  "  + num1 +"  and " +num2 +" is: "+sub+"</br>");
+
+var num1=3;
+var num2=5;
+
+var product=num1*num2;
+
+document.write(" product of  "  + num1 +"  and " +num2 +" is: "+product+"</br");
+
